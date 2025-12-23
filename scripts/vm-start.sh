27f0acf2b983e01031d03d2903d476930d7ce42d
Alex Luong <alex@alexluong.com>
2025-12-24 02:08:45 +0700

    chore: move media hard drive

--- scripts/vm-start.sh
+++ scripts/vm-start.sh
@@ -6,7 +6,7 @@ colima start arr \
   --cpu 2 \
   --memory 2 \
   --disk 10 \
-  --mount "/Volumes/T7/arr:w" \
+  --mount "/Volumes/Blue4/arr:w" \
   --mount-type virtiofs \
   --vm-type vz \
   --runtime docker
